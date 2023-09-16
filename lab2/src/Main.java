cc80537af2324b6b641719ff1fbcc0ab081d9546
rejchev <hendlerq@yandex.ru>
2023-09-16 07:31:32 +0500

    RSA: file stream added

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,63 +1,113 @@
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.Random;
 
 // var 11
 public class Main {
 
-    public static final int KeyBitLength = 32;
+    public static final int KeyBitLength = 512;
 
-    public static final int Certainty = 1;
+    public static final RSA.Decoder DecoderInstance = RSA.getDecoder();
 
-    public static final byte[] Source = "ABCD. 123. Съешь ещё этих мягких французских булочек"
-            .getBytes();
+    public static final RSA.Encoder EncoderInstance = RSA.getEncoder();
+
+    public static final RSAKeyPairGenerator.RSAKeyPair Keys = genKeys(KeyBitLength);
+
+    public static final String ToFilePath = "/data/some.txt";
 
     public static void main(String[] args) {
-        Optional<RSAKeyPairGenerator.RSAKeyPair> value = RSA.getRSAKeyPair(
-                KeyBitLength, Certainty, new SecureRandom()
-        );
 
-        if(value.isEmpty())
-            return;
-
-        System.out.println("Public key bits: " + value.get().getPublicKey().getPublicExponent().bitLength());
-        System.out.println("Private key bits: " + value.get().getPrivateKey().getPrivateExponent().bitLength());
-
-        int bits = value.get().getModulus().bitLength();
-        int blockSize = bits/Byte.SIZE;
-        int blockCount = Source.length/blockSize + 1;
-
-        byte[] buffer;
-        byte[] encoded;
         byte[] decoded = new byte[0];
-        for(int i = 0, end; i < blockCount; i++) {
 
-            buffer = new byte[blockSize];
+        int iBlockInBytes = getBlockSizeInBytes(Keys.getModulus().bitLength());
 
-            end = (i * blockSize) + blockSize;
+        try(FileInputStream inputStream = new FileInputStream(getAbsolutePathToFile())) {
 
-            if(end > Source.length)
-                end = Source.length;
+            int iBytes;
+            int iOffset = 0;
 
-            System.arraycopy(Source, i * blockSize, buffer, 0, end - (i * blockSize));
+            byte[] encoded;
+            byte[] buffer = new byte[iBlockInBytes];
 
-            encoded = RSA.getEncoder().encode(buffer, value.get().getPublicKey());
+            while((iBytes = inputStream.readNBytes(buffer, iOffset, Math.min(iBlockInBytes, inputStream.available()))) > 0) {
 
-            System.out.printf("Encoded block #%d: %s\n", i + 1, Arrays.toString(encoded));
+                if(iBytes != iBlockInBytes)
+                    buffer = Arrays.copyOf(buffer, iBlockInBytes);
 
-            buffer = RSA.getDecoder().decode(encoded, value.get().getPrivateKey());
+                encoded = encode(buffer, EncoderInstance, Keys.getPublicKey());
 
-            System.out.printf("Decoded block #%d: %s\n", i + 1, Arrays.toString(buffer));
+                decoded = copyWithResizeCustom(decoded, decode(encoded, DecoderInstance, Keys.getPrivateKey()));
 
-            decoded = Arrays.copyOf(decoded, decoded.length + buffer.length);
-
-            System.arraycopy(buffer, 0, decoded, decoded.length - buffer.length, buffer.length);
+                buffer = new byte[iBlockInBytes];
+            }
 
             System.out.println();
-        }
 
-        System.out.println("source: " + new String(Source, StandardCharsets.UTF_8));
-        System.out.println("decoded: " + new String(decoded, StandardCharsets.UTF_8).replaceAll("\\x00", ""));
+        } catch (IOException ignored) {        }
+
+        System.out.println("Decoded: \n" + new String(decoded, StandardCharsets.UTF_8).replaceAll("\\x00", ""));
+    }
+
+    static byte[] copyWithResizeCustom(byte[] source, byte[] value) {
+
+        source = Arrays.copyOf(source, source.length + value.length);
+
+        System.arraycopy(value, 0, source, source.length - value.length, value.length);
+
+        return source;
+    }
+
+    static String getAbsolutePathToFile() {
+        return System.getProperty("user.dir") +  ToFilePath;
+    }
+
+    static int getBlockSizeInBytes(int realKeyBits) {
+        return realKeyBits / Byte.SIZE;
+    }
+
+    static int getTotalBlocks(byte[] buffer, int blockSize) {
+        return buffer.length / blockSize + 1;
+    }
+
+    public static byte[] encode(byte[] value, RSA.Encoder encoder, RSAPublicKey key) {
+
+        byte[] encoded = encoder.encode(value, key);
+
+        System.out.printf("Source: %s; \nSourceInBytes: %s (total: %d) \nEncodedInBytes: %s (total: %d); \nEncoded: %s\n\n",
+                new String(value, StandardCharsets.UTF_8),
+                Arrays.toString(value), value.length,
+                Arrays.toString(encoded), encoded.length,
+                new String(encoded, StandardCharsets.UTF_8));
+
+        return encoded;
+    }
+
+    public static byte[] decode(byte[] value, RSA.Decoder decoder, RSAPrivateKey key) {
+
+        byte[] decoded = decoder.decode(value, key);
+
+        System.out.printf("Source: %s; \nSourceInBytes: %s (total: %d) \nDecodedInBytes: %s (total: %d); \nDecoded: %s\n\n",
+                new String(value, StandardCharsets.UTF_8),
+                Arrays.toString(value), value.length,
+                Arrays.toString(decoded), decoded.length,
+                new String(decoded, StandardCharsets.UTF_8));
+
+        return decoded;
+    }
+
+    public static RSAKeyPairGenerator.RSAKeyPair genKeys(int keyBitLength) {
+        Optional<RSAKeyPairGenerator.RSAKeyPair> buffer;
+        Random rnd = new SecureRandom();
+
+        do buffer = RSA.getRSAKeyPair(keyBitLength, 1, rnd);
+        while(buffer.isEmpty());
+
+        return buffer.get();
     }
 }
